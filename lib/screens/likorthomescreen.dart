e9ebb5c6fe984465da7fc0083770b11a3c779682
Buteya <buteyamasira@gmail.com>
2024-11-05 18:22:55 +0300

    image size

--- lib/screens/likorthomescreen.dart
+++ lib/screens/likorthomescreen.dart
@@ -14,44 +14,40 @@ class LikortHomeScreen extends StatefulWidget {
 class _LikortHomeScreenState extends State<LikortHomeScreen> {
   @override
   Widget build(BuildContext context) {
+    // Get the screen size
+    var screenSize = MediaQuery.of(context).size;
+    // Get the screen orientation
+    var orientation = MediaQuery.of(context).orientation;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          itemCount: 100,
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: (){
-                Navigator.of(context).pushNamed('/likortproductdetail');
-              },
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 6.0),
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemCount: 100,
+            itemBuilder: (context, index) {
+              return  Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   Image.network(
-                        'https://cdn.pixabay.com/photo/2016/09/20/18/49/brushes-1683134_1280.jpg',
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height/2,
-                      ),
-                   const Column(
-                      mainAxisSize: MainAxisSize.min,
-                     mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Steve`s store'),
-                        Text('painting'),
-                        Text('brushes'),
-                        Text('\$1200'),
-                      ],
-                    ),
+                    'https://cdn.pixabay.com/photo/2016/09/20/18/49/brushes-1683134_1280.jpg',
+                    width: screenSize.width,
+                    height: screenSize.height * 0.4,
+                  ),
+                  const Text('Steve`s store'),
+                  const Text('painting'),
+                  const Text('brushes'),
+                  const Text('\$1200'),
                 ],
-              ),
-            );
-          }),
+              );
+            }),
+      ),
     );
   }
 }
