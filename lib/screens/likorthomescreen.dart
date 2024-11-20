61128ba4c8ff6c11b58244dc633618add6f6af6b
Buteya <buteyamasira@gmail.com>
2024-11-20 20:45:55 +0300

    ui changes homescreen

--- lib/screens/likorthomescreen.dart
+++ lib/screens/likorthomescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LikortHomeScreen extends StatefulWidget {
   const LikortHomeScreen({
@@ -16,18 +17,61 @@ class LikortHomeScreen extends StatefulWidget {
 }
 
 class _LikortHomeScreenState extends State<LikortHomeScreen> {
+  final _searchController = TextEditingController();
+
+  List<String> _items = List<String>.generate(100, (index) => 'Item $index');
+  late List<String> _filteredItems;
+  @override
+  void initState() {
+    super.initState();
+    _filteredItems = _items;
+  }
+
+  void _filterItems(String query) {
+    setState(() {
+      _filteredItems = _items
+          .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // Get the screen size
     var screenSize = MediaQuery.of(context).size;
     // Get the screen orientation
     var orientation = MediaQuery.of(context).orientation;
+    final bool isDark = Theme.of(context).brightness == Brightness.dark;
 
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 74,
+        leading: const Icon(
+          Icons.dehaze_rounded,
+          size: 40,
+        ),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Transform.rotate(
+              angle: -6 * 3.1415926535897932 / 180,
+              child: Transform.scale(
+                scaleX: 1.36,
+                scaleY: 1.0,
+                child: Text(
+                  widget.title,
+                  style: GoogleFonts.dancingScript(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
         actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
           InkWell(
             onTap: () {
               Navigator.of(context).pushNamed('/likortuserprofile');
@@ -45,48 +89,87 @@ class _LikortHomeScreenState extends State<LikortHomeScreen> {
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
-        child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            ),
-            itemCount: 100,
-            itemBuilder: (context, index) {
-              return InkWell(
-                onTap: () {
-                  Navigator.of(context).pushNamed('/likortproductdetail');
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Image.network(
-                      'https://cdn.pixabay.com/photo/2016/09/20/18/49/brushes-1683134_1280.jpg',
-                      width: screenSize.width,
-                      height: orientation == Orientation.portrait
-                          ? screenSize.height * 0.1
-                          : screenSize.height * 0.36,
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 6.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: PreferredSize(
+                preferredSize: const Size.fromHeight(48.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      hintStyle: TextStyle(
+                        color: isDark ? Colors.white54 : Colors.black54,
+                      ),
+                      hintText: 'Search...',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      filled: true,
+                      fillColor: isDark ? Colors.grey[800] : Colors.grey[200],
+                      prefixIcon: Icon(
+                        Icons.search,
+                      ),
                     ),
-                    const SizedBox(height: 7.0),
-                    const Text('title',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                    const Text('\$price',
-                        style: TextStyle(fontSize: 16, color: Colors.green)),
-                    const Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.star, color: Colors.orange, size: 20),
-                        Text('rating (reviewCount reviews)'),
-                      ],
-                    ),
-                    const SizedBox(height: 5.0),
-                    const Text('description')
-                  ],
+                    onChanged: (query) {
+                      _filterItems(query);
+                    },
+                  ),
                 ),
-              );
-            }),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 100,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.of(context).pushNamed('/likortproductdetail');
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15.0),
+                              child: Image.network(
+                                'https://cdn.pixabay.com/photo/2016/09/20/18/49/brushes-1683134_1280.jpg',
+                                width: screenSize.width * .83,
+                                height: screenSize.height / 2.66,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 7.0),
+                          const Text('title',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold)),
+                          const Text('\$price',
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.green)),
+                          const Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.star, color: Colors.orange, size: 20),
+                              Text('rating (reviewCount reviews)'),
+                            ],
+                          ),
+                          const SizedBox(height: 5.0),
+                          const Text('description')
+                        ],
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/likortsplashscreen.dart
+++ lib/screens/likortsplashscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LikortSplashScreen extends StatefulWidget {
   const LikortSplashScreen({super.key});
@@ -7,16 +8,47 @@ class LikortSplashScreen extends StatefulWidget {
   State<LikortSplashScreen> createState() => _LikortSplashScreenState();
 }
 
-class _LikortSplashScreenState extends State<LikortSplashScreen> {
+class _LikortSplashScreenState extends State<LikortSplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _shakeAnimation;
+  late Animation<double> _sizeAnimation;
   @override
   void initState() {
     super.initState();
     _navigateToLogin();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _shakeAnimation = Tween<double>(begin: 0, end: 20).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.elasticIn),
+    );
+    _sizeAnimation = Tween<double>(begin: 1.0, end: 1.5).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
+      }
+    });
+    _startAnimation();
+    super.initState();
   }
 
   _navigateToLogin() async {
-    await Future.delayed(const Duration(seconds: 3), () {});
+    await Future.delayed(const Duration(seconds: 4), () {});
     Navigator.pushReplacementNamed(context, '/likortlogin');
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _startAnimation() {
+    _controller.forward();
   }
 
   @override
@@ -27,14 +59,36 @@ class _LikortSplashScreenState extends State<LikortSplashScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // Add your splash screen content here
-            Text(
-              'Likort',
-              style: Theme.of(context).textTheme.displayLarge,
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Transform.translate(
+                  offset: Offset(_shakeAnimation.value, 0),
+                  child: Transform.scale(
+                    scale: _sizeAnimation.value,
+                    child: child,
+                  ),
+                );
+              },
+              child: Transform.rotate(
+                angle: -6 * 3.1415926535897932 / 180,
+                child: Transform.scale(
+                  scaleX: 1.36,
+                  scaleY: 1.0,
+                  child: Text(
+                    'Likort',
+                    style: GoogleFonts.dancingScript(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(24.0),
               child: SizedBox(
-                width: MediaQuery.of(context).size.width*.14,
+                width: MediaQuery.of(context).size.width * .14,
                 child: const LinearProgressIndicator(),
               ),
             ), // Optional loading indicator
